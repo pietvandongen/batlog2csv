cfc74a8d7edeb331daf837beaa430311446495c2
Piet van Dongen <piet@studio-donder.nl>
2014-08-08 16:52:40 +0200

    Formatted tests for nosetest, added Travis CI information

--- tests/testconvert.py
+++ tests/testconvert.py
@@ -1,48 +1,49 @@
 import csv
-import unittest
+import os
+from os.path import dirname, abspath
 
 from batlog2csv import Batlog2Csv
 
-
-class TestConvert(unittest.TestCase):
-    def test_that_standard_log_is_parsed_correctly(self):
-        csv = self.get_csv("data/default.dat")
-
-        header = csv.next()
-        row = csv.next()
-
-        self.check_header(header)
-        self.assertEqual(row[0], "2012-08-14 10:41:46")
-        self.assertEqual(row[1], "1")
-        self.assertEqual(row[2], "6667")
-        self.assertEqual(row[3], "6329")
-        self.assertEqual(row[4], "6700")
-
-    def test_that_standard_log_is_parsed_correctly(self):
-        csv = self.get_csv("data/wrongorder.dat")
-
-        header = csv.next()
-        row = csv.next()
-
-        self.check_header(header)
-        self.assertEqual(row[0], "2014-08-07 00:15:00")
-        self.assertEqual(row[1], "19")
-        self.assertEqual(row[2], "8532")
-        self.assertEqual(row[3], "8532")
-        self.assertEqual(row[4], "8440")
-
-    def check_header(self, header):
-        self.assertEqual(header[0], "Date")
-        self.assertEqual(header[1], "CycleCount")
-        self.assertEqual(header[2], "MaxCapacity")
-        self.assertEqual(header[3], "CurrentCapacity")
-        self.assertEqual(header[4], "DesignCapacity")
-
-    def get_csv(self, file_path):
-        with open(file_path) as data_file:
-            return csv.reader(Batlog2Csv(
-                data_file.read().splitlines()).convert().strip().split("\n"))
+baseDir = dirname(abspath(__file__)) + os.path.sep
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_that_standard_log_is_parsed_correctly():
+    table = get_csv(baseDir + "/data/default.dat")
+
+    header = table.next()
+    row = table.next()
+
+    check_header(header)
+    assert row[0] == "2012-08-14 10:41:46"
+    assert row[1] == "1"
+    assert row[2] == "6667"
+    assert row[3] == "6329"
+    assert row[4] == "6700"
+
+
+def test_that_wrongly_ordered_log_is_parsed_correctly():
+    table = get_csv(baseDir + "/data/wrongorder.dat")
+
+    header = table.next()
+    row = table.next()
+
+    check_header(header)
+    assert row[0] == "2014-08-07 00:15:00"
+    assert row[1] == "19"
+    assert row[2] == "8532"
+    assert row[3] == "8532"
+    assert row[4] == "8440"
+
+
+def check_header(header):
+    assert header[0] == "Date"
+    assert header[1] == "CycleCount"
+    assert header[2] == "MaxCapacity"
+    assert header[3] == "CurrentCapacity"
+    assert header[4] == "DesignCapacity"
+
+
+def get_csv(file_path):
+    with open(file_path) as data_file:
+        return csv.reader(Batlog2Csv(
+            data_file.read().splitlines()).convert().strip().split("\n"))
--- tests/testparsedate.py
+++ tests/testparsedate.py
@@ -3,78 +3,73 @@ import unittest
 from batlog2csv import Batlog2Csv
 
 
-class TestParseDate(unittest.TestCase):
-    def test_that_standard_date_is_parsed_correctly(self):
-        date_string = "Mon Sep 12 13:29:00 CDT 2013"
-        year = 2013
-        month = 9
-        day = 12
-        hour = 13
-        minute = 29
-        second = 0
+def test_that_standard_date_is_parsed_correctly():
+    date_string = "Mon Sep 12 13:29:00 CDT 2013"
+    year = 2013
+    month = 9
+    day = 12
+    hour = 13
+    minute = 29
+    second = 0
 
-        parsed_date, key, is_date = Batlog2Csv.parse_date(date_string)
+    parsed_date, key, is_date = Batlog2Csv.parse_date(date_string)
 
-        self.assertEqual(is_date, True)
-        self.assertEqual(parsed_date.year, year)
-        self.assertEqual(parsed_date.month, month)
-        self.assertEqual(parsed_date.day, day)
-        self.assertEqual(parsed_date.hour, hour)
-        self.assertEqual(parsed_date.minute, minute)
-        self.assertEqual(parsed_date.second, second)
+    assert is_date == True
+    assert parsed_date.year == year
+    assert parsed_date.month == month
+    assert parsed_date.day == day
+    assert parsed_date.hour == hour
+    assert parsed_date.minute == minute
+    assert parsed_date.second == second
 
-    def test_that_date_with_timezone_as_last_part_is_parsed_correctly(self):
-        date_string = "Fri 16 Aug 2013 21:47:02 BST"
-        year = 2013
-        month = 8
-        day = 16
-        hour = 21
-        minute = 47
-        second = 2
+def test_that_date_with_timezone_as_last_part_is_parsed_correctly():
+    date_string = "Fri 16 Aug 2013 21:47:02 BST"
+    year = 2013
+    month = 8
+    day = 16
+    hour = 21
+    minute = 47
+    second = 2
 
-        parsed_date, key, is_date = Batlog2Csv.parse_date(date_string)
+    parsed_date, key, is_date = Batlog2Csv.parse_date(date_string)
 
-        self.assertEqual(is_date, True)
-        self.assertEqual(parsed_date.year, year)
-        self.assertEqual(parsed_date.month, month)
-        self.assertEqual(parsed_date.day, day)
-        self.assertEqual(parsed_date.hour, hour)
-        self.assertEqual(parsed_date.minute, minute)
-        self.assertEqual(parsed_date.second, second)
+    assert is_date == True
+    assert parsed_date.year == year
+    assert parsed_date.month == month
+    assert parsed_date.day == day
+    assert parsed_date.hour == hour
+    assert parsed_date.minute == minute
+    assert parsed_date.second == second
 
-    def test_that_standard_datetime_string_is_parsed_correctly(self):
-        date_string = "2013-11-05 18:11:00"
-        year = 2013
-        month = 11
-        day = 5
-        hour = 18
-        minute = 11
-        second = 0
+def test_that_standard_datetime_string_is_parsed_correctly():
+    date_string = "2013-11-05 18:11:00"
+    year = 2013
+    month = 11
+    day = 5
+    hour = 18
+    minute = 11
+    second = 0
 
-        parsed_date, key, is_date = Batlog2Csv.parse_date(date_string)
+    parsed_date, key, is_date = Batlog2Csv.parse_date(date_string)
 
-        self.assertEqual(is_date, True)
-        self.assertEqual(parsed_date.year, year)
-        self.assertEqual(parsed_date.month, month)
-        self.assertEqual(parsed_date.day, day)
-        self.assertEqual(parsed_date.hour, hour)
-        self.assertEqual(parsed_date.minute, minute)
-        self.assertEqual(parsed_date.second, second)
+    assert is_date == True
+    assert parsed_date.year == year
+    assert parsed_date.month == month
+    assert parsed_date.day == day
+    assert parsed_date.hour == hour
+    assert parsed_date.minute == minute
+    assert parsed_date.second == second
 
-    def test_that_non_date_string_is_not_converted_to_date(self):
-        date_string = "123"
-        parsed_date, key, is_date = Batlog2Csv.parse_date(date_string)
+def test_that_non_date_string_is_not_converted_to_date():
+    date_string = "123"
+    parsed_date, key, is_date = Batlog2Csv.parse_date(date_string)
 
-        self.assertEqual(parsed_date, None)
-        self.assertEqual(is_date, False)
+    assert parsed_date == None
+    assert is_date == False
 
-    def test_that_none_value_is_not_converted_to_date(self):
-        date_string = None
-        parsed_date, key, is_date = Batlog2Csv.parse_date(date_string)
+def test_that_none_value_is_not_converted_to_date():
+    date_string = None
+    parsed_date, key, is_date = Batlog2Csv.parse_date(date_string)
 
-        self.assertEqual(parsed_date, None)
-        self.assertEqual(is_date, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    assert parsed_date == None
+    assert is_date == False
