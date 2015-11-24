74f6c989bbe5440ba037c12bb959c64ae3f5192d
hugovk <hugovk@users.noreply.github.com>
2015-11-24 20:10:42 +0200

    Add passing test for EET and failing test for EEST

--- tests/test_date_parser.py
+++ tests/test_date_parser.py
@@ -41,6 +41,46 @@ def test_that_date_with_timezone_as_last_part_is_parsed_correctly():
     assert parsed_date.second == second
 
 
+def test_that_date_with_eet_timezone_is_parsed_correctly():
+    date_string = "Sat Mar 28 12:58:00 EET 2015"
+    year = 2015
+    month = 3
+    day = 28
+    hour = 12
+    minute = 58
+    second = 0
+
+    key, parsed_date, is_date = Batlog2Csv.parse_date(date_string)
+
+    assert is_date
+    assert parsed_date.year == year
+    assert parsed_date.month == month
+    assert parsed_date.day == day
+    assert parsed_date.hour == hour
+    assert parsed_date.minute == minute
+    assert parsed_date.second == second
+
+
+def test_that_date_with_eest_timezone_is_parsed_correctly():
+    date_string = "Sun Mar 29 10:28:00 EEST 2015"
+    year = 2015
+    month = 3
+    day = 29
+    hour = 10
+    minute = 28
+    second = 0
+
+    key, parsed_date, is_date = Batlog2Csv.parse_date(date_string)
+
+    assert is_date
+    assert parsed_date.year == year
+    assert parsed_date.month == month
+    assert parsed_date.day == day
+    assert parsed_date.hour == hour
+    assert parsed_date.minute == minute
+    assert parsed_date.second == second
+
+
 def test_that_standard_datetime_string_is_parsed_correctly():
     date_string = "2013-11-05 18:11:00"
     year = 2013
